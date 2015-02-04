9a01a2432f308fdcc7aaa2c242516a8fde2b16f3
Vladimir Kocjancic <kovalenkov@yahoo.com>
2015-02-04 21:36:39 +0100

    Initial commit

---
+++ CertSerialNumberExtractor.Library/Utils/CommonNameParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CertSerialNumberExtractor.Library.Utils {
+    
+    internal class CommonNameParser {
+
+        #region Static methods
+
+        public static string GetSimpleName( string fqdn ) {
+            var name = GetPart( fqdn, "CN=", "," );
+            if ( null != name )
+                return name;
+            name = GetPart( fqdn, "O=", "," );
+            if ( null != name )
+                return name;
+            return fqdn;
+        }
+
+        private static string GetPart( string src, string part, string separator ) {
+            var startLocation = src.IndexOf( part );
+            if ( startLocation == -1 )
+                return null;
+            startLocation += part.Length;
+            var separatorLocation = src.IndexOf( separator, startLocation );
+            if ( separatorLocation == -1 )
+                return src.Substring( startLocation );
+            return src.Substring( startLocation, separatorLocation - startLocation );
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ CertSerialNumberExtractor.Library/ViewModel/Certificate.cs
@@ -0,0 +1,46 @@
+﻿using CertSerialNumberExtractor.Library.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CertSerialNumberExtractor.Library.ViewModel {
+    
+    public class Certificate {
+
+        #region Properties
+
+        public string ExpirationDate { get; set; }
+        public string IssuedTo { get; set; }
+        public string IssuedBy { get; set; }
+        public string SerialNumber { get; set; }
+
+        #endregion
+
+        #region Static methods
+
+        public static List<Certificate> GetAllFromStore(StoreLocation location) {
+            var certificates = new List<Certificate>();
+            var store = new X509Store( location );
+            store.Open( OpenFlags.ReadOnly );
+            foreach ( var cert in store.Certificates ) {
+                certificates.Add( Certificate.CreateFromX509Certificate( cert ) );
+            }
+            return certificates;
+        }
+
+        private static Certificate CreateFromX509Certificate( X509Certificate2 cert ) {
+            var certVM = new Certificate();
+            certVM.ExpirationDate = cert.GetExpirationDateString();
+            certVM.IssuedBy = CommonNameParser.GetSimpleName( cert.IssuerName.Name );
+            certVM.IssuedTo = CommonNameParser.GetSimpleName( cert.SubjectName.Name );
+            certVM.SerialNumber = cert.SerialNumber;
+            return certVM;
+        }
+
+        #endregion
+
+    }
+
+}
